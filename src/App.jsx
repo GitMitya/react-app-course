7e1f3da998cdf479cad8d2329618b39d0a2bf3e3
Dmitry Naumov <komrad.nda@yandex.ru>
2026-08-29 13:57:56 +0300

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import { MainLayout } from './components/MainLayout'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { MainLayout } from './components/MainLayout/MainLayout'
 
 function App() {
-	return <MainLayout />
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route element={<MainLayout />}>
+					<Route path="/" element={<div>Home Content</div>} />
+					<Route path="/forbidden" element={<div>Forbidden Content</div>} />
+					<Route path="/addquestion" element={<div>Add Question Content</div>} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	)
 }
 
 export default App
