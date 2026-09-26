76cd2fcf40ead45426f1db66cfe82af3dbadecd8
Dmitry Naumov <komrad.nda@yandex.ru>
2026-09-26 16:43:43 +0300

    add sort

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { Loader } from '../../components/Loader/Loader'
 import { QuestionCardList } from '../../components/QuestionCardList'
 import { SearchInput } from '../../components/SearchInput/SearchInput,.jsx'
@@ -9,6 +9,7 @@ import cls from './HomePage.module.css'
 export const HomePage = () => {
 	const [questions, setQuestions] = useState([])
 	const [searchValue, setSearchValue] = useState('')
+	const [sortSelectValue, setSortSelectValue] = useState('')
 
 	const [getQuestions, isLoading, error] = useFetch(async (url) => {
 		const response = await fetch(`${API_URL}/${url}`)
@@ -17,22 +18,38 @@ export const HomePage = () => {
 		return data
 	})
 
+	const cards = useMemo(() => {
+		return questions.filter((data) => data.question.toLowerCase().includes(searchValue.trim().toLowerCase()))
+	}, [questions, searchValue])
+
 	useEffect(() => {
-		getQuestions('react')
-	}, [])
+		getQuestions(`react?${sortSelectValue}`)
+	}, [sortSelectValue])
 
 	const onSearchChangeHandler = (e) => {
 		setSearchValue(e.target.value)
+	}
+	const onSortSelectChangeHandler = (e) => {
+		setSortSelectValue(e.target.value)
 	}
 
 	return (
 		<>
 			<div className={cls.controlsContainer}>
 				<SearchInput value={searchValue} onChange={onSearchChangeHandler} />
+				<select value={sortSelectValue} onChange={onSortSelectChangeHandler} className={cls.select}>
+					<option value="sort by">sort by</option>
+					<hr />
+					<option value="_sort=level">level ASC</option>
+					<option value="_sort=-level">level DESC</option>
+					<option value="_sort=completed">completed ASC</option>
+					<option value="_sort=-completed">completed DESC</option>
+				</select>
 			</div>
 			{isLoading && <Loader />}
 			{error && <p>{error}</p>}
-			<QuestionCardList cards={questions} />
+			{cards.length === 0 && <p className={cls.noCardsInfo}>No questions found</p>}
+			<QuestionCardList cards={cards} />
 		</>
 	)
 }
